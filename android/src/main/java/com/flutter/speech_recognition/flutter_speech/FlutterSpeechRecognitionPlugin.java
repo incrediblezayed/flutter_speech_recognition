f6762a78cb340a092abb0ac069799c9c8cb945fb
Hassan Ansari <hassanansari222@gmail.com>
2022-05-16 16:32:20 +0530

    Update FlutterSpeechRecognitionPlugin.java

--- android/src/main/java/com/flutter/speech_recognition/flutter_speech/FlutterSpeechRecognitionPlugin.java
+++ android/src/main/java/com/flutter/speech_recognition/flutter_speech/FlutterSpeechRecognitionPlugin.java
@@ -42,7 +42,7 @@ public class FlutterSpeechRecognitionPlugin implements FlutterPlugin, ActivityAw
     recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
     recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
             RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-    recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false);
+    recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
     recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
 
     recognizerIntent.putExtra(
